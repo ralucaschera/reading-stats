6e13c5979634383d9cf0c582422a72335b881bb9
ralucaschera <raluca.schera@webamboos.com>
2020-07-03 15:57:45 +0300

    created app and set up service worker

---
+++ build/precache-manifest.140c538b322eefdf9784b223713e868f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50c80834663915b44b51e9a54f815e60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71ea76612be8d0ca3557",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "97697aaf7187dc9f38c1",
+    "url": "/static/js/2.a205cc5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a205cc5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71ea76612be8d0ca3557",
+    "url": "/static/js/main.2c0ca6d6.chunk.js"
+  },
+  {
+    "revision": "7e099ead9fe8ca022908",
+    "url": "/static/js/runtime-main.941caaf0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
